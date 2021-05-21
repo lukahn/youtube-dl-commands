1bf055e2a88c510c8ec7fed83e4760f54117a2c2
Luke Wakefield <lukew3@gmail.com>
2021-05-21 21:32:39 +1000

    Added script to download Bluey

---
+++ bluey-download.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# For downloading Bluey. Put the list in list.txt, and append .mp4 to the filename.
+IFS=$'\n'
+for line in $(cat list.txt); do
+	episode=$(echo $line | cut -d "," -f 1)
+	echo $episode
+	filename=$(echo $line | cut -d "," -f 2 | sed 's/\"//g')
+	echo $filename
+	youtube-dl -v "$episode" --all-subs --embed-subs --add-metadata --embed-thumbnail --output "$filename"
+	result=$?
+	if [ $result -eq 1 ]; then
+		echo $line >> unavailable.txt
+	fi
+done
